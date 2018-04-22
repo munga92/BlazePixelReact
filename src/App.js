7e94e4131d78da0641a88af0008d86e4a2f88f6e
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-04-21 23:37:08 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import RouterSpark from './router'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <RouterSpark />
+      </div>
+    )
+  }
+}
+
+export default App
