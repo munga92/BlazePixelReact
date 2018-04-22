daadf3a5a5338d6d947f7f5082d8e4f0ea807dd4
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-04-22 18:39:14 -0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import './App.css'
 import RouterSpark from './router'
 
 class App extends Component {
+  componentWillMount () {
+  }
+
   render () {
     return (
       <div className='App'>
--- src/router.js
+++ src/router.js
@@ -7,15 +7,31 @@ import {
 import Home from './pages/home.js'
 import About from './pages/about.js'
 
+import logo from './assets/img/logo.png'
+
 class RouterSpark extends Component {
   render () {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-          </ul>
+          <header className='main_h sticky'>
+            <div className='row'>
+              <a className='logo' href='#'>
+                <img src={logo} />
+              </a>
+              <div className='mobile-toggle'>
+                <span />
+                <span />
+                <span />
+              </div>
+              <nav>
+                <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/about'>About</Link></li>
+                </ul>
+              </nav>
+            </div>
+          </header>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
         </div>
