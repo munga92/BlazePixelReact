dffa4c3c64bbe4b1aa1153c3f91c1eb1938f84ea
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-04-24 22:21:10 -0500

    Test

--- src/router.js
+++ src/router.js
@@ -11,27 +11,40 @@ import logo from './assets/img/logo.png'
 
 class RouterSpark extends Component {
   render () {
+    var navbar =  ''
+    // var navbar =  (<a className='navbar-item' href='https://bulma.io'>
+    //   <img src='https://bulma.io/images/bulma-logo.png'
+    //     alt='Bulma: a modern CSS framework based on Flexbox'
+    //     width='112' height='28' />
+    // </a>)
     return (
       <Router>
         <div>
-          <header className='main_h sticky'>
-            <div className='row'>
-              <a className='logo' href='#'>
-                <img src={logo} />
-              </a>
-              <div className='mobile-toggle'>
+          <nav className='navbar is-transparent'>
+            <div className='navbar-brand'>
+              {navbar}
+              <div className='navbar-burger burger'
+                data-target='navbarExampleTransparentExample'>
                 <span />
                 <span />
                 <span />
               </div>
-              <nav>
-                <ul>
-                  <li><Link to='/'>Home</Link></li>
-                  <li><Link to='/about'>About</Link></li>
-                </ul>
-              </nav>
             </div>
-          </header>
+
+            <div id='navbarExampleTransparentExample' className='navbar-menu'>
+              <div className='navbar-start'>
+                <Link to='/' className='navbar-item'>Home</Link>
+                <Link to='/about' className='navbar-item'>About</Link>
+              </div>
+
+              <div className='navbar-end'>
+                <div className='navbar-item'>
+                  <div className='field is-grouped'>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
         </div>
